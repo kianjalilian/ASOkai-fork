beed6a16a28dbf2083066f7ec80b8760f2d68843
arash.ayatollahi <arashayat@gmail.com>
2025-06-08 20:03:30 +0200

    Add __all__ declaration in __init__.py to define public API for module

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
 from .src import * 
+
+__all__ = [
+    "CandidateTargetsManager",
+    "KmerCounter",
+    "ResultsGenerator",
+    "GenomeDataManager",
+    "PedersenAnalysis",
+    "SecondarySiteFinder",
+    "RNACofold",
+    "ProgressTracker",
+    "GenomeUtils",
+    "format_duration",
+    "GenomeUtils",
+]
